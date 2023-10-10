4516658f442fb595805857ae877adb95525d2574
uyenmit <tranphuonguyen0622@gmail.com>
2023-10-10 23:09:13 +0700

    update

---
+++ bai_tap.js
@@ -0,0 +1,16 @@
+let x = 4;
+let y = 2;
+
+let result = x + y;
+console.log("🚀 ~ file: bai_tap.js:4 ~ result:", result)
+
+let result1 = x % y; // chia lấy phần dư
+console.log("🚀 ~ file: bai_tap.js:6 ~ result1:", result1)
+
+let result2 = x**y // x mũ y
+let result3 = Math.pow(x,y)
+console.log("🚀 ~ file: bai_tap.js:12 ~ result3:", result3)
+console.log("🚀 ~ file: bai_tap.js:11 ~ result2:", result2)
+
+let result4 = Math.sqrt(x,y) // x can bac y
+console.log("🚀 ~ file: bai_tap.js:16 ~ result4:", result4)
---
+++ index.js
@@ -0,0 +1,72 @@
+//console.log("heloo")
+/**
+ * comment 1 đoạn code dài
+ */
+// khai báo biến
+// let, var, const
+// let, var khai báo 1 biến và gắn lại giá trị cho biến đó
+//let x = 0;
+// if (true) {
+//     var x = 0;
+//     console.log("🚀 ~ file: index.js:11 ~ x:", x)
+// }
+// console.log(x);
+// var x = 10;
+// x = 20;
+// console.log("🚀 ~ file: index.js:16 ~ x:", x)
+// let y = 100;
+// console.log("🚀 ~ file: index.js:18 ~ y:", y)
+// const c = 10 // dùng để đặt tên biến ko bio thay đồi (tên, )
+// console.log("🚀 ~ file: index.js:20 ~ c:", c)
+
+// c = 20;
+
+//kieu du lieu
+
+// string
+// let a = "10";
+// console.log("🚀 ~ file: index.js:29 ~ a:", typeof a)
+
+// number
+// let g = 10;
+// console.log("🚀 ~ file: index.js:29 ~ g:", typeof g)
+
+// boolean
+// let y = true
+// console.log("🚀 ~ file: index.js:35 ~ y:", typeof y)
+
+// object
+//id: {}
+
+//null, Undefined
+//let x = null // biến thuộc kiểu dữ liệu kiểu dữ liệu cụ thể hoặc ko tổn tại
+
+// let user = {
+//     aa: {},
+//     bb: "123123",
+//     name: "uyen",
+//     age: 18,
+//     thu_muc: false,
+// }
+// console.log("🚀 ~ file: index.js:51 ~ user:", typeof user)
+
+// let arr = [1,2,3,4,54,54];
+// console.log("🚀 ~ file: index.js:54 ~ arr:", Array.isArray (arr));
+
+//IF_ELSE
+let age = 15;
+if ( age >= 18) {
+    console.log("Bạn đã đủ tuổi bầu cử");
+} else if (age >= 15) {
+    console.log(" Đủ tuổi")
+} else {
+    console.log("bạn còn bé")
+}
+
+//ifelse + enter
+if (condition) {
+
+} else {
+    
+}
+
